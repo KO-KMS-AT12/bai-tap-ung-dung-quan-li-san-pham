8c099a2a876a5be0581bf30a29479a2871cb3a31
KO-KMS-AT12 <vtg0502@gmail.com>
2019-04-11 09:33:02 +0700

    complete show, edit, del, add

--- main.js
+++ main.js
@@ -4,34 +4,52 @@ let valueProduct = document.getElementById("products");
 let nameProduct = document.getElementById("txtProduct");
 let countProduct = document.getElementById("countProduct");
 
+
 function showProduct() {
     for (let i = 0; i < products.length; i++) {
         valueProduct.innerHTML +=
             '<tr>'
             + '<td>' + products[i] + '</td>'
-            + '<td><input type="button" onclick="editProduct(' + i + ')" value="Edit"/></td>'
-            + '<td><input type="button" onclick="delProduct(' + i + ')"value="Delete"/></td>'
+            + '<td><input type="button" onclick="edit(' + i + ')" value="Edit"/></td>'
+            + '<td><input type="button" onclick="del(' + i + ')" value="Delete"/></td>'
             + '</tr>';
         count++
     }
     countProduct.innerHTML = count;
 }
 
-function addProduct() {
+function add() {
     refersh();
     products.push(nameProduct.value);
     showProduct();
 }
 
 
-function delProduct(item) {
+function del(item) {
     refersh();
     products.splice(item, item + 1);
     showProduct();
 }
 
-function editProduct(item) {
 
+function edit(item) {
+    let editProduct = document.getElementById("editProduct");
+    let btnSave = document.getElementById("btnSave");
+    let btnCancel = document.getElementById("btnCancel");
+
+    editProduct.placeholder = products[item];
+
+    show(editProduct, btnSave, btnCancel);
+
+    btnCancel.onclick = function () {
+        hidden(editProduct, btnSave, btnCancel);
+    };
+    btnSave.onclick = function () {
+        products[item] = editProduct.value;
+        refersh();
+        showProduct();
+        hidden(editProduct, btnSave, btnCancel);
+    };
 }
 
 function refersh() {
@@ -39,3 +57,14 @@ function refersh() {
     count = 0;
 }
 
+function show(editProduct, btnSave, btnCancel) {
+    editProduct.style.visibility = "visible";
+    btnSave.style.visibility = "visible";
+    btnCancel.style.visibility = "visible";
+}
+
+function hidden(editProduct, btnSave, btnCancel) {
+    editProduct.style.visibility = "hidden";
+    btnSave.style.visibility = "hidden";
+    btnCancel.style.visibility = "hidden";
+}
